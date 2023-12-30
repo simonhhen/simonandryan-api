3ad30250c03ba344c05c639eca8360c4f003bb85
simonhhen <simon_henderson@rogers.com>
2023-12-30 09:24:46 -0500

    no more deletes

--- api/routes/item.js
+++ api/routes/item.js
@@ -4,9 +4,7 @@ module.exports = (app) => {
   app.route('/items')
     .get(itemList.list_all_items)
     .post(itemList.create_a_item)
-    .delete(itemList.delete_all_items)
   app.route('/items/:itemId')
     .get(itemList.read_a_item)
-    .put(itemList.update_a_item)
-    
+    .put(itemList.update_a_item)  
 };
